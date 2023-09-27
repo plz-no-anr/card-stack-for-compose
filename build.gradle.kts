4ebaeeff4c5cd60de9505a007c601b20b1169259
sg.park <psgxxx@nm-metaworld.com>
2023-09-27 11:33:14 +0900

    [Add] - 라이센스 및 퍼블리싱 정보 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,29 @@
 import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
 
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.com.android.application) apply false
     alias(libs.plugins.org.jetbrains.kotlin.android) apply false
     alias(libs.plugins.com.android.library) apply false
+    alias(libs.plugins.io.github.gradle.nexus.publish)
+}
+
+val properties = gradleLocalProperties(rootDir)
+
+val userMap = mapOf(
+    "ossrhUsername" to properties.getProperty("ossrhUsername"),
+    "ossrhPassword" to properties.getProperty("ossrhPassword"),
+    "sonatypeStagingProfileId" to properties.getProperty("sonatypeStagingProfileId"),
+)
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            stagingProfileId.set(userMap["sonatypeStagingProfileId"])
+            username.set(userMap["ossrhUsername"])
+            password.set(userMap["ossrhPassword"])
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+        }
+    }
 }
--- card-stack/build.gradle.kts
+++ card-stack/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.com.android.library)
     alias(libs.plugins.org.jetbrains.kotlin.android)
+    `version-catalog`
+    `maven-publish`
+    signing
 }
 
 android {
@@ -39,4 +42,79 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.espresso.core)
+}
+
+val properties = com.android.build.gradle.internal.cxx.configure.gradleLocalProperties(rootDir)
+val signingMap = mapOf(
+    "signing.keyId" to properties.getProperty("signing.keyId"),
+    "signing.password" to properties.getProperty("signing.password"),
+    "signing.key" to properties.getProperty("signing.key")
+)
+
+val androidSourceJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("sources")
+    from(android.sourceSets.getByName("main").java.srcDirs,)
+}
+
+android.publishing.singleVariant("release")
+
+val publishGroup = "io.github.plz-no-anr"
+val publishArtifact = "card-stack"
+val publishVersion = "1.0.0"
+
+group = publishGroup
+version = publishVersion
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                if (project.plugins.hasPlugin("com.android.library")) {
+                    from(components["release"])
+                } else {
+                    from(components["java"])
+                }
+                artifact(androidSourceJar.get())
+                groupId = publishGroup
+                artifactId = publishArtifact
+                version = publishVersion
+                // Mostly self-explanatory metadata
+                pom {
+                    name.set("card-stack-for-compose")
+                    description.set("Card Stack for Compose")
+                    url.set("https://github.com/plz-no-anr/card-stack-for-compose")
+                    licenses {
+                        license {
+                            name.set("MIT license")
+                            url.set("https://opensource.org/license/mit")
+                        }
+                    }
+                    developers {
+                        developer {
+                            id.set("plz-no-anr")
+                            name.set("SanggunPark")
+                            email.set("psgxxx@naver.com")
+                        }
+                    }
+                    // Version control info
+                    scm {
+                        connection.set("scm:git:github.com/plz-no-anr/card-stack-for-compose.git")
+                        developerConnection.set("scm:git:ssh://github.com/plz-no-anr/card-stack-for-compose.git")
+                        url.set("https://github.com/plz-no-anr/card-stack-for-compose/tree/main")
+                    }
+                }
+
+            }
+
+        }
+    }
+}
+
+signing {
+    useInMemoryPgpKeys(
+        signingMap["signing.keyId"],
+        signingMap["signing.key"],
+        signingMap["signing.password"]
+    )
+    sign(publishing.publications)
 }
